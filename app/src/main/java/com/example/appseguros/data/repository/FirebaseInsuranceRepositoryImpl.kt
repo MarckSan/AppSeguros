421058e998b8b76397c63c00a31d1a80a865cd6f
MarckSan <85895353+MarckSan@users.noreply.github.com>
2025-03-26 23:13:56 -0600

    feature product info
    se crea la parte de ver los productos de información

--- app/src/main/java/com/example/appseguros/data/repository/FirebaseInsuranceRepositoryImpl.kt
+++ app/src/main/java/com/example/appseguros/data/repository/FirebaseInsuranceRepositoryImpl.kt
@@ -1,11 +1,15 @@
 package com.example.appseguros.data.repository
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.compose.ui.input.key.key
+import androidx.core.view.children
 import com.example.appseguros.data.model.ContractedInsurance
 import com.example.appseguros.data.model.InsuranceProduct
 import com.example.appseguros.data.model.PdfData
 import com.example.appseguros.data.model.User
+import com.example.appseguros.utilitiesGeneral.UtilitiesFun
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.tasks.await
 import java.time.LocalDate
@@ -15,16 +19,17 @@ import javax.inject.Inject
 class FirebaseInsuranceRepositoryImpl @Inject constructor(
     private val database: FirebaseDatabase
 ) : InsuranceRepository {
-    private val productsRef = database.getReference("products")
+    private val productsRef = database.getReference("insuranceProducts")
     private val contractedRef = database.getReference("contractedInsurances")
     private val usersRef = database.getReference("users")
     private val pdfRef = database.getReference("pdfUrl")
 
     override suspend fun getInsuranceProducts(): List<InsuranceProduct> {
         val snapshot = productsRef.get().await()
-        return snapshot.children.mapNotNull {
-            it.getValue(InsuranceProduct::class.java)
+        val lista : List<InsuranceProduct> = snapshot.children.mapNotNull { childSnapshot ->
+            childSnapshot.getValue(InsuranceProduct::class.java)?.copy(id = childSnapshot.key ?: "")
         }
+        return lista
     }
 
     override suspend fun getContractedInsurances(userId: Int): List<ContractedInsurance> {
@@ -42,18 +47,25 @@ class FirebaseInsuranceRepositoryImpl @Inject constructor(
     }
 
     override suspend fun saveUser(user: User): Boolean {
-        val userId = usersRef.push().key
+        val userId = UtilitiesFun.extractUsernameSplit(user.email?: "")
         return userId?.let {
-            usersRef.child(it).setValue(user.copy(id = it)).isSuccessful
+            val task = usersRef.child(it).setValue(user.copy(id = it))
+            try {
+                task.await() // Espera a que la tarea se complete
+                task.isSuccessful // Verifica el resultado DESPUÉS de que la tarea se complete
+            } catch (e: Exception) {
+                // Manejo de errores si la tarea falla
+                println("Error al guardar el usuario: ${e.message}")
+                false
+            }
         } ?: false
     }
 
     override suspend fun loginUser(email: String, pass: String): User? {
-        val snapshot = usersRef.orderByChild("email").equalTo(email).get().await()
-        val user = snapshot.children.mapNotNull {
-            it.getValue(User::class.java)
-        }.firstOrNull()
-        return user?.takeIf { it.pass == pass }
+        //val snapshot = usersRef.orderByChild("email").equalTo(email).get().await() //Busca por el correo
+        val snapshot = usersRef.child(UtilitiesFun.extractUsernameSplit(email)).get().await()
+        val user = snapshot.getValue(User::class.java)
+        return user?.takeIf { it.pass == pass } //Verifica la contraseña
     }
 
     override suspend fun getPdfUrl(): PdfData {
